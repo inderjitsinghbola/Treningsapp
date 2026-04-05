0036d1c178a094d7de7d350dfc2d2f756da2d332
inderjitsinghbola <inderjit.singh.bola@gmail.com>
2026-04-05 15:46:51 +0200

    Update fix-quotes.cjs

--- fix-quotes.cjs
+++ fix-quotes.cjs
@@ -1,12 +1,15 @@
 const fs = require(`fs`);
 const files = [`src/App.jsx`];
 files.forEach(file => {
-let c = fs.readFileSync(file, `utf8`);
-c = c
-.replace(/\u201c/g, `\u0022`)
-.replace(/\u201d/g, `\u0022`)
-.replace(/\u2018/g, `\u0027`)
-.replace(/\u2019/g, `\u0027`);
-fs.writeFileSync(file, c);
-console.log(`Fixed: ` + file);
-}); 
+  let c = fs.readFileSync(file, `utf8`);
+  const dot = `.`;
+  const dots = dot + dot + dot;
+  c = c
+    .replace(/\u201c/g, `\u0022`)
+    .replace(/\u201d/g, `\u0022`)
+    .replace(/\u2018/g, `\u0027`)
+    .replace(/\u2019/g, `\u0027`)
+    .replace(/\u2026/g, dots);
+  fs.writeFileSync(file, c);
+  console.log(`Fixed: ` + file);
+});
