ae37048f6a4f75b0010615b10c39084ab08e2ed6
inderjitsinghbola <inderjit.singh.bola@gmail.com>
2026-04-05 15:18:26 +0200

    Create fix-quotes.js

---
+++ fix-quotes.js
@@ -0,0 +1,12 @@
+const fs = require(`fs`);
+const files = [`src/App.jsx`];
+files.forEach(file => {
+let c = fs.readFileSync(file, `utf8`);
+c = c
+.replace(/\u201c/g, `\u0022`)
+.replace(/\u201d/g, `\u0022`)
+.replace(/\u2018/g, `\u0027`)
+.replace(/\u2019/g, `\u0027`);
+fs.writeFileSync(file, c);
+console.log(`Fixed: ` + file);
+});
